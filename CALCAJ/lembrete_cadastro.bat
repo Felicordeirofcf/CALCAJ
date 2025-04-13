58d9e874db387712f9760170c589609ba6f31b96
Felipe Cordeiro Ferreira <felicordeirofcf@gmail.com>
2025-04-13 03:41:09 -0300

    Delete CALCAJ directory

--- CALCAJ/lembrete_cadastro.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo Lembrete: Você tem um cálculo judicial com prazo para amanhã!
-pause
--- CALCAJ/utils/helpers.py
+++
@@ -1,4 +0,0 @@
-def gerar_callback_exclusao(id_, janela_principal):
-    def callback():
-        janela_principal.excluir_cliente(id_)
-    return callback
